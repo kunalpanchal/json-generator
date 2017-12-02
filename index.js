694fb813322cbba25657903582b5c2f6d0822223
kay <hello@kunalpanchal.in>
2017-12-02 19:20:54 +0530

    update the index

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
 'use strict';
 
-var port = process.env.PORT || 8080;
-var cluster = require('cluster');
-var numCPUs = require('os').cpus().length;
-var app = require('express')();
-var fs = require('fs');
+const cluster = require('cluster');
+const fs = require('fs');
+const shelljs = require('shelljs');
+
+const port = process.env.PORT || 8080;
+const numberOfClusters = 100;
+const recordsPerFile = 500;
+const uploadToES = true;
+const ES_URL = 'localhost:9200';
+const ES_INDEX = 'testtt';
 
 if (cluster.isMaster) {
-    for (var i = 0; i < 90; i++) {
-        var worker = cluster.fork();
-    }
+    for (let i = 0; i < numberOfClusters; i++)
+        cluster.fork();
+
+    process.on('beforeExit', code => uploadToES && updateDataToES());
+
 } else {
-    let dict = fs.readFileSync('dict.txt', 'utf8').toString().split('\n');
-    let workerId = cluster.worker.id;
+    const dict = fs.readFileSync('dict.txt', 'utf8').toString().split('\n');
+    const workerId = cluster.worker.id;
     let data = '';
-    for (var i = 0; i < 100000; i++) {
+
+    for (let i = 0; i < recordsPerFile; i++)
         data += createFakeData(workerId, i, dict);
-    }
-    fs.writeFile(`data/myjsonfile${workerId}.json`, data, 'utf8', () => {
-       console.log(`json created for ${workerId}`)
-       cluster.worker.destroy();
+
+    !fs.existsSync('data') && fs.mkdirSync('data');
+
+    fs.writeFile(`data/json-data-${workerId}.json`, data, 'utf8', () => {
+        console.log(`json created for ${workerId}`)
+        cluster.worker.destroy();
     });
+
 }
 
+function updateDataToES() {
+    shelljs.exec(` 
+    for i in $(seq ${numberOfClusters})
+    do  
+      echo $i
+      curl -H 'Content-Type: application/x-ndjson' -XPOST '${ES_URL}/${ES_INDEX}/doc/_bulk?pretty' --data-binary @data/json-data-$i.json >/dev/null;
+    done`)
+}
 
 function createFakeData(workerId, i, dict) {
+    const storyLength = 20;
     const index = {
         "index": {
             "_id": `${workerId}${i}`
         }
     };
+
     let story = '', title = '';
 
-    for (let i = 0; i < 20; i++)
+    for (let i = 0; i < storyLength; i++)
         story += ' ' + dict[Math.floor(Math.random() * dict.length)];
 
     const body = {
